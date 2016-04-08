39f13868dd508ac452053818ac7d23dfa168ff0b
kyoyababa <amour.le.jour.ideal@gmail.com>
2016-04-08 20:23:53 +0900

    deleted built files

--- assets/js/constants.js
+++
@@ -1,1 +0,0 @@
-const SCHEDULE_D=["Title","Start","End","Day","Duration","Description","Where"],LIST_D=["month","day"],MONTH_NAMES=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],WEEKDAY=["SUN","MON","TUE","WED","THU","FRI","SAT"],MS_PER_DAY=864e5;
